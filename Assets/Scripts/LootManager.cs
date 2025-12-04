f25f0a8f37730deabc7de257b9c8d9569f86bdd3
Fabian-RB <fabianes2002@gmail.com>
2025-12-04 01:49:01 -0300

    Enemigos sueltan loot
    Los enemigos sueltan monedas y los grandes sueltan monedas y pociones que curan 20 de salud (25% de probabilidad), solo el mapa sigue bug

---
+++ Assets/Scripts/LootManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using TMPro; // <--- NECESARIO PARA EL TEXTO
+
+public class LootManager : MonoBehaviour
+{
+    public static LootManager Instance;
+
+    [Header("Objetos que van a caer")]
+    public GameObject monedaPrefab;
+    public GameObject pocionPrefab;
+
+    [Header("Interfaz (UI)")]
+    public TextMeshProUGUI textoMonedas; // <--- ARRASTRA AQUÍ TU TEXTO NUEVO
+    public int monedasTotales = 0;
+
+    public enum EnemyType { Basico, Charger }
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        // Al empezar, ponemos el contador a 0 en la pantalla
+        ActualizarTexto();
+    }
+
+    public void SpawnLoot(Vector3 posicionMuerte, EnemyType tipoEnemigo)
+    {
+        // Forzamos Z=0 para intentar arreglar lo visual
+        Vector3 posicionVisible = new Vector3(posicionMuerte.x, posicionMuerte.y, 0f);
+
+        if (tipoEnemigo == EnemyType.Basico)
+        {
+            float probabilidad = Random.Range(0f, 100f);
+            if (probabilidad <= 70f)
+            {
+                Instantiate(monedaPrefab, posicionVisible, Quaternion.identity);
+            }
+        }
+        else if (tipoEnemigo == EnemyType.Charger)
+        {
+            Instantiate(monedaPrefab, posicionVisible, Quaternion.identity);
+
+            float probabilidadPocion = Random.Range(0f, 100f);
+            // Puedes dejar esto en 25f o subirlo para pruebas
+            if (probabilidadPocion <= 25f)
+            {
+                Instantiate(pocionPrefab, posicionVisible, Quaternion.identity);
+            }
+        }
+    }
+
+    public void SumarMoneda(int cantidad)
+    {
+        monedasTotales += cantidad;
+        ActualizarTexto(); // Actualizamos la pantalla
+        Debug.Log("💰 MONEDAS: " + monedasTotales);
+    }
+
+    // Función auxiliar para escribir en pantalla
+    void ActualizarTexto()
+    {
+        if (textoMonedas != null)
+        {
+            textoMonedas.text = "Monedas: " + monedasTotales.ToString();
+        }
+    }
+}
